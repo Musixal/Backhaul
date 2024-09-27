a432bfa2b5cec1db11e2cfc8dc43e4540826f49b
Musixal <Musixal@users.noreply.github.com>
2024-09-27 18:16:14 +0330

    use io.copy for better performance

--- internal/utils/tcpConnHandler.go
+++ internal/utils/tcpConnHandler.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"errors"
 	"io"
 	"net"
 
@@ -9,7 +8,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func ConnectionHandler(from net.Conn, to net.Conn, logger *logrus.Logger, usage *web.Usage, remotePort int, sniffer bool) {
+func TCPConnectionHandler(from net.Conn, to net.Conn, logger *logrus.Logger, usage *web.Usage, remotePort int, sniffer bool) {
 	done := make(chan struct{})
 
 	go func() {
@@ -25,45 +24,19 @@ func ConnectionHandler(from net.Conn, to net.Conn, logger *logrus.Logger, usage 
 	to.Close()
 }
 
-// Using direct Read and Write for transferring data
+// Using io.Copy for efficient data transfer
 func transferData(from net.Conn, to net.Conn, logger *logrus.Logger, usage *web.Usage, remotePort int, sniffer bool) {
-	buf := make([]byte, 16*1024) // 16K
-	for {
-		// Read data from the source connection
-		r, err := from.Read(buf)
-		if err != nil {
-			if errors.Is(err, io.EOF) || errors.Is(err, net.ErrClosed) {
-				logger.Trace("reader stream closed or EOF received")
-			} else {
-				logger.Trace("unable to read from the connection: ", err)
-			}
-
-			from.Close()
-			to.Close()
-			return
-		}
-
-		totalWritten := 0
-		for totalWritten < r {
-			// Write data to the destination connection
-			w, err := to.Write(buf[totalWritten:r])
-			if err != nil {
-				if errors.Is(err, net.ErrClosed) {
-					logger.Trace("writer stream closed or EOF received")
-				} else {
-					logger.Trace("unable to write to the connection: ", err)
-				}
-				from.Close()
-				to.Close()
-				break
-			}
-			totalWritten += w
-		}
-
-		logger.Tracef("read data: %d bytes, written data: %d bytes", r, totalWritten)
-		if sniffer {
-			go usage.AddOrUpdatePort(remotePort, uint64(totalWritten))
-		}
+	bytesCopied, err := io.Copy(to, from)
+	if err != nil {
+		logger.Trace("error during data transfer: ", err)
 	}
 
+	logger.Tracef("data transferred: %d bytes", bytesCopied)
+
+	if sniffer {
+		usage.AddOrUpdatePort(remotePort, uint64(bytesCopied))
+	}
+
+	from.Close()
+	to.Close()
 }
