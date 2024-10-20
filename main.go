fcaf92ca0ec83f0d291151b9b2cac442108b1293
Musixal <Musixal@users.noreply.github.com>
2024-10-20 13:47:40 +0330

    bump version 0.6.2

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ var (
 )
 
 // Define the version of the application
-const version = "v0.6.1"
+const version = "v0.6.2"
 
 func getLastModTime(file string) (time.Time, error) {
 	absPath, _ := filepath.Abs(file)
